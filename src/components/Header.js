b71907772eb3316f4f52fc302fc968a30b9a7365
unknown <gianna.belen.russo@gmail.com>
2021-09-07 23:51:00 -0300

    [ADD] Ciclo de vida

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import { Component } from "react";
 import img from "../img/gigi.jpg";
+import "animate.css";
 
 class Header extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = { text: "gia-developer", class: "" }
+    }
+
+    componentDidMount( ) {
+        setInterval(() => {
+            this.setState( { text: "parcial-react" } );
+        }, 3000);
+    }
+
     render( ) {
         return (
             <header>
-                <h1>💗 gia-developer</h1>
+                <h1 class="animate__animated animate__bounce">💗 { this.state.text }</h1>
                 <img src={ img } alt="Gianna Russo" />
             </header>
         )
